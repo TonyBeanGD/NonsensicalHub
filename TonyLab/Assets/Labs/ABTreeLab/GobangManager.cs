80c686fb1a67a93670cf84ec56b96e1b21e19fef
Tony <2479445653@qq.com>
2020-08-14 19:31:14 +0800

    8.14 TonyGD 1.五子棋 2.对象池

--- TonyLab/Assets/Labs/ABTreeLab/GobangManager.cs
+++ TonyLab/Assets/Labs/ABTreeLab/GobangManager.cs
@@ -43,34 +43,7 @@ public class GobangManager
     private float evaluation(List<Int2> my_list, List<Int2> enemy_list)
     {
         float total_score = 0;
-        
-        List<int[]> score_all_arr = 0;
 
-        float my_score = 0;
-        foreach (var pt in my_list)
-        {
-            int m = pt.int1;
-            int n = pt.int2;
-            my_score += cal_score(m, n, 0, 1, enemy_list, my_list, score_all_arr);
-            my_score += cal_score(m, n, 1, 0, enemy_list, my_list, score_all_arr);
-            my_score += cal_score(m, n, 1, 1, enemy_list, my_list, score_all_arr);
-            my_score += cal_score(m, n, -1, 1, enemy_list, my_list, score_all_arr);
-        }
-
-        float score_all_arr = 0;
-        enemy_score = 0
-    for pt in enemy_list:
-        m = pt[0]
-        n = pt[1]
-        enemy_score += cal_score(m, n, 0, 1, my_list, enemy_list, score_all_arr_enemy)
-        enemy_score += cal_score(m, n, 1, 0, my_list, enemy_list, score_all_arr_enemy)
-        enemy_score += cal_score(m, n, 1, 1, my_list, enemy_list, score_all_arr_enemy)
-        enemy_score += cal_score(m, n, -1, 1, my_list, enemy_list, score_all_arr_enemy)
-
-    total_score = my_score - enemy_score * ratio * 0.1
-
-    return total_score
+        return total_score;
     }
-  
-
 }
