9ebbc8d009a4ca40274a98eae17418092e8c791a
Tony <2479445653@qq.com>
2021-02-04 11:18:09 +0800

    截屏

---
+++ TonyLab/Assets/BaseLabs/CaptureLab/CaptureLab.cs
@@ -0,0 +1,15 @@
+﻿using NonsensicalKit.Utility;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.XR.WSA.WebCam;
+
+public class CaptureLab : MonoBehaviour
+{
+    void Start()
+    {
+        ScreenCapture.CaptureScreenshot(Path.Combine(Application.streamingAssetsPath, "Game.png"));
+    }
+}
